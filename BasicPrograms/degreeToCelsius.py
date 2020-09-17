e43d8a75bd65bbfa5b4a06be6435a8a6871b203a
noob <shelcia1999@gmail.com>
2020-09-18 02:10:47 +0530

    update first program

---
+++ BasicPrograms/degreeToCelsius.py
@@ -0,0 +1,5 @@
+# A PYTHON PROGRAM TO CONVERT DEGREES TO CELSIUS
+# MY FIRST PROGRAM
+celsius = float(input("enter degree in celsius: "))
+farenheit = (celsius*1.8)+32
+print("%0.2f celsius equals %0.2f farenheit" % (celsius, farenheit))
