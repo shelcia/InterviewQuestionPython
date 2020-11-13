04ed9ca675f9b2bcd346aff3fe09f42a83214869
Shelcia abi <msm17b011@iiitdm.ac.in>
2020-11-13 23:13:45 +0530

    update insertion

---
+++ LinkedLists/LinkedInIntro.py
@@ -0,0 +1,37 @@
+# CREATING LINKEDLIST
+# 1.VALUE
+# 2.NEXT NODE
+
+
+class linkedListNode:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.nextNode = nextNode
+
+
+class linkedList:
+    # FUNCTION TO PRINT THE LINKEDLIST
+    def printList(self, node1):
+        currentNode = node1
+        while True:
+            print(currentNode.value, "->", end="")
+            if(currentNode.nextNode is None):
+                print("None")
+                break
+            currentNode = currentNode.nextNode
+
+
+# DRIVER FUNCTION
+
+if __name__ == '__main__':
+
+    node1 = linkedListNode("2")
+    node2 = linkedListNode("11")
+    node3 = linkedListNode("1999")
+
+    node1.nextNode = node2
+    node2.nextNode = node3
+    node3.nextNode = None
+
+    printList = linkedList()  # CREATE INSTANCE
+    printList.printList(node1)  # CALL THE FUNCTION
