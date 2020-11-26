4f82644cea59883407b60f93b94824f5f19f9f06
Shelcia abi <msm17b011@iiitdm.ac.in>
2020-11-26 10:33:10 +0530

    update

---
+++ LinkedLists/FindLengthOfLoop.py
@@ -0,0 +1,11 @@
+#FIND LENGTH OF THE LOOP
+
+class linkedListNode:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.nextNode = nextNode
+
+
+class linkedList:
+    def __init__(self, head=None):
+        self.head = head
