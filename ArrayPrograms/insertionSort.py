f9485d1aa92fb0869baba099b84f1fe37804e330
shelcia <shelcia1999@gmail.com>
2021-01-27 22:33:54 +0530

    update

--- ArrayPrograms/insertionSort.py
+++ ArrayPrograms/insertionSort.py
@@ -1,17 +1,24 @@
+# INSERTION SORT
 
+# COMPARE THE PREDECESSOR WITH KEY, IF LESS SWAP WITH THE ELEMENT AND SO ON
 
 def insertionSort(arr):
 
-
-    
     for index in range(1, len(arr)):
-        
 
+        key = arr[index]
 
+        j = index - 1
+
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+        # print(index, arr)
+    return arr
 
 
 if __name__ == "__main__":
 
     print(insertionSort([45, 78, 3, 67, 90, 0, 12]))
-
-
--- ArrayPrograms/minmaxOfArray.py
+++ ArrayPrograms/minmaxOfArray.py
@@ -1,17 +1,17 @@
 # FINDING SMALLEST AND LARGEST INDEXES
-N = int(input(""))
-Array = list(map(int, input().split(' ')[:N]))
+# N = int(input(""))
+# Array = list(map(int, input().split(' ')[:N]))
+Array = [32, 14, 12, 98, 24, 56, 76, 34]
 max = Array[0]
 min = Array[0]
 maxIndex = 0
 minIndex = 0
-for i in range(1, N):
+for i in range(1, len(Array)):
     if(max < Array[i]):
         max = Array[i]
         maxIndex = i
     elif(min > Array[i]):
         min = Array[i]
         minIndex = i
-# print(max)
-# print(min)
-print(minIndex+1, maxIndex+1)
+print("Index", minIndex+1, maxIndex+1)
+print(min, max)
